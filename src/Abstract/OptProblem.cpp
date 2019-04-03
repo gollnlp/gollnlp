63a4824c90f2c37fb2ede66c3a320c4e01d83dea
Cosmin G. Petra <petra1@llnl.gov>
2019-04-03 16:15:47 -0700

    sketchy sketch of the abstract Opt objects

--- src/Abstract/OptProblem.cpp
+++ src/Abstract/OptProblem.cpp
@@ -1,5 +1,11 @@
 #include "OptProblem.hpp"
 
+#include <iostream>
+
+#include "blasdefs.hpp"
+
+using namespace std;
+
 namespace gollnlp {
 
 OptProblem::OptProblem(OptVariables* vars)
@@ -11,7 +17,7 @@ OptProblem::~OptProblem()
 {
   for(auto& t: m_objterms) delete t;
   for(auto& c: m_conblocks) delete c;
-  delete m_vars; 
+  //delete m_vars; 
 }
 
 bool OptProblem::eval_obj(double* x, double& obj_val)
@@ -39,7 +45,7 @@ bool OptProblem::eval_gradobj (double* x, double* grad)
       return false;
   return true;
 }
-bool OptProblem::eval_Jaccons (double* x, const size_t& nnz, int* i, int* j, double* M)
+bool OptProblem::eval_Jaccons (double* x, const int& nnz, int* i, int* j, double* M)
 {
   m_vars->attach_to(x);
   //! here we need to call eval_deriv with correct jumps in i,j, and M
@@ -49,7 +55,7 @@ bool OptProblem::eval_Jaccons (double* x, const size_t& nnz, int* i, int* j, dou
   return true;
 }
 //! mulipliers
-bool OptProblem::eval_HessLagr(double* x, const size_t& nnz, int* i, int* j, double* M)
+bool OptProblem::eval_HessLagr(double* x, const int& nnz, int* i, int* j, double* M)
 {
   //!
   m_vars->attach_to(x);
@@ -62,4 +68,69 @@ bool OptProblem::eval_HessLagr(double* x, const size_t& nnz, int* i, int* j, dou
   return true;
 }
 
+OptVariables::OptVariables()
+  : n(0) {}
+
+
+OptVariables::~OptVariables()
+{
+  for(auto b: m_vblocks)
+    delete b;
 }
+bool OptVariables::append_varsblock(OptVarsBlock* b)
+{
+  if(b) {
+    if(m_mblocks.find(b->id)!= m_mblocks.end()) {
+      cerr << "appendVarsBlock: cannot add block since its identifier has been already used" << endl;
+      assert(false);
+      return false;
+    }
+    m_vblocks.push_back(b);
+    m_mblocks[b->id] = b;
+    b->index=this->n;
+    this->n += b->n;
+  }
+}
+
+void OptVariables::attach_to(double *x)
+{
+  for(auto b: m_vblocks) b->x = x + b->index;
+}
+
+OptVariables::OptVarsBlock::OptVarsBlock(const int& n_, const std::string& id_)
+  : n(n_), id(id_), index(-1), x(NULL)
+{
+  assert(n>=0);
+  int i;
+  lb = new double[n];
+  for(i=0; i<n; i++) lb[i] = -1e+20;
+
+  ub = new double[n];
+  for(i=0; i<n; i++) ub[i] = +1e+20;
+}
+
+OptVariables::OptVarsBlock::OptVarsBlock(const int& n_, const std::string& id_, double* lb_, double* ub_)
+  : n(n_), id(id_), index(-1), x(NULL)
+{
+  assert(n>=0);
+  int i, one=1;
+  lb = new double[n];
+  if(lb_)
+    DCOPY(&n, lb, &one, lb_, &one);
+  else
+    for(i=0; i<n; i++) lb[i] = -1e+20;
+
+  ub = new double[n];
+  if(ub_)
+    DCOPY(&n, ub, &one, ub_, &one);
+  else
+    for(i=0; i<n; i++) ub[i] = +1e+20;
+}
+
+OptVariables::OptVarsBlock::~OptVarsBlock()
+{
+  delete[] lb;
+  delete[] ub;
+}
+
+} //end of namespace
--- src/Abstract/OptProblem.hpp
+++ src/Abstract/OptProblem.hpp
@@ -4,49 +4,58 @@
 #include <string>
 #include <cassert>
 #include <vector>
+#include <map>
 namespace gollnlp {
 
 class OptVariables {
 public:
-  OptVariables(int num, double lb=-1e+20, double ub=+1e+20);
-  OptVariables(int num, double* lb=NULL, double* ub=NULL);
+  OptVariables();
+  //create on OptVarsBlock with no bounds with id "main"
+  //OptVariables(const int& n);
+
   virtual ~OptVariables();
-  
-  virtual void attach_to(double* x) = 0;
+
+  struct OptVarsBlock {
+    OptVarsBlock(const int& n_, const std::string& id_);
+    OptVarsBlock(const int& n_, const std::string& id_, double* lb, double* ub=NULL);
+    virtual ~OptVarsBlock();
+    //number of vars in the block
+    int n; 
+    // index at which the block starts within OptVariables
+    int index;
+    // identifier, unique within OptVariables; is maintained by OptVariables
+    std::string id; 
+    //pointer/reference to the first elem in "x" that corresponds to this block
+    double* x;
+    //lower and upper vector bounds -> these are allocated and deallocated by this class
+    double *lb, *ub;
+  };
+  // appends b to list of blocks; updates this->n and b->index
+  bool append_varsblock(OptVarsBlock* b);
+
+  virtual void attach_to(double* x);
 public:
-  double *lb, *ub;
-  size_t size;
+  int n;
+protected:
+  // "list" of pointers to blocks
+  std::vector<OptVarsBlock*> m_vblocks;
+  // "dict" with the pointers for quick lookup
+  std::map<std::string, OptVarsBlock*> m_mblocks;
 private:
-  OptVariables() : size(-1), lb(NULL), ub(NULL) {};
+
 };
   
 class OptDerivativeEval {
 public:
   virtual bool eval_body (const OptVariables& x, double* body) = 0;
   virtual bool eval_deriv(const OptVariables& x, double* grad) = 0;
-  virtual bool eval_deriv(const OptVariables& x, const size_t& nnz, 
+  virtual bool eval_deriv(const OptVariables& x, const int& nnz, 
 			  int* i, int* j, double* M) = 0;
-  virtual bool eval_Hess(const OptVariables& x, const size_t& nnz, 
+  virtual bool eval_Hess(const OptVariables& x, const int& nnz, 
 			 int* i, int* j, double* M) = 0;
 };
 
-class OptConstraintsBlock : public OptDerivativeEval {
-public:
-  OptConstraintsBlock();
-  virtual ~OptConstraintsBlock();
-  virtual bool eval_body (const OptVariables& x, double* body);
-  virtual bool eval_deriv(const OptVariables& x, double* grad)
-  {
-    assert(false && "constraints cannot provide dense derivatives");
-    return false;
-  }
-  virtual bool eval_deriv(const OptVariables& x, const size_t& nnz, int* i, int* j, double* M);
-  virtual bool eval_Hess(const OptVariables& x, const size_t& nnz,  int* i, int* j, double* M);
-public: 
-  size_t offset, size;
-  int id;
-};
-
+// Holds objective terms to be minimized
 class OptObjectiveTerm : public OptDerivativeEval {
 public:
   OptObjectiveTerm() : sense(1) {};
@@ -56,18 +65,56 @@ public:
   virtual ~OptObjectiveTerm();
   virtual bool eval_body (const OptVariables& x, double* body);
   virtual bool eval_deriv(const OptVariables& x, double* grad);
-  virtual bool eval_deriv(const OptVariables& x, const size_t& nnz, int* i, int* j, double* M)
+  virtual bool eval_deriv(const OptVariables& x, const int& nnz, int* i, int* j, double* M)
   {
     assert(false && "objective cannot provide Jacobians");
     return false;
   }
-  virtual bool eval_Hess(const OptVariables& x, const size_t& nnz, int* i, int* j, double* M);
+  virtual bool eval_Hess(const OptVariables& x, const int& nnz, int* i, int* j, double* M);
+};
+
+class OptConstraintsBlock : public OptDerivativeEval {
+public:
+  OptConstraintsBlock();
+  virtual ~OptConstraintsBlock();
+
+  // some constraints needs to create additional variables (e.g., slacks)
+  // this method is called by OptProblem to get the constraints' varblock whenever
+  // the constraints are added to the OptProblem (via append_constraints method)
+  // NULL should be returned when the constraints need not create a vars block
+  virtual OptVariables::OptVarsBlock* create_varsblock() { return NULL; }
+
+  //same as above. OptProblem calls this when its append_objterm is called
+  virtual OptObjectiveTerm* create_objterm() { return NULL; }
+
+  virtual bool eval_body (const OptVariables& x, double* body);
+  virtual bool eval_deriv(const OptVariables& x, double* grad)
+  {
+    assert(false && "constraints cannot provide dense derivatives");
+    return false;
+  }
+  virtual bool eval_deriv(const OptVariables& x, const int& nnz, int* i, int* j, double* M);
+  virtual bool eval_Hess(const OptVariables& x, const int& nnz,  int* i, int* j, double* M);
+public: 
+  // number of constraints
+  int n;
+  //index where the block starts in the constraints
+  int index;
+  // identifier - the block number, starting at 1
+  int id;
 };
 
 class OptProblem {
 public:
   OptProblem(OptVariables* vars);
   virtual ~OptProblem();
+
+  inline int get_num_constraints() const 
+  {
+    return m_conblocks.size()>0 ? m_conblocks.back()->index + m_conblocks.back()->n : 0;
+  }
+
+  inline int get_num_variables() const { return m_vars->n; }
 
 protected:
   OptVariables*  m_vars;
@@ -78,21 +125,25 @@ public:
   virtual bool eval_obj     (double* x, double& obj);
   virtual bool eval_cons    (double* x, double* cons);
   virtual bool eval_gradobj (double* x, double* grad);
-  virtual bool eval_Jaccons (double* x, const size_t& nnz, int* i, int* j, double* M);
-  //! mulipliers
-  virtual bool eval_HessLagr(double* x, const size_t& nnz, int* i, int* j, double* M);
+  virtual bool eval_Jaccons (double* x, const int& nnz, int* i, int* j, double* M);
+  //! multipliers
+  virtual bool eval_HessLagr(double* x, const int& nnz, int* i, int* j, double* M);
 
-  inline void add_conblock(OptConstraintsBlock* con) { 
+  inline void append_constraints(OptConstraintsBlock* con) { 
     if(con) {
+      con->index = this->get_num_constraints();
       m_conblocks.push_back(con); 
       m_conblocks.back()->id = m_conblocks.size();
+      
+      m_vars->append_varsblock(con->create_varsblock());
+      m_objterms.push_back(con->create_objterm());
     } else assert(con);
   }
-  inline void add_objterm(OptObjectiveTerm* obj) { 
+  inline void append_objterm(OptObjectiveTerm* obj) { 
     if(obj) {
       m_objterms.push_back(obj);
       m_objterms.back()->id = m_objterms.size();
-    } else assert(obj);
+    } 
   }
   
 private:
