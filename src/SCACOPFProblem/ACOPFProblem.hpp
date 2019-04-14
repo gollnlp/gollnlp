4758209ff45868dc89dd1a983408be40770c082b
Cosmin G. Petra <petra1@llnl.gov>
2019-04-14 10:33:46 -0700

    base case with power flow constraints in

--- src/SCACOPFProblem/ACOPFProblem.hpp
+++ src/SCACOPFProblem/ACOPFProblem.hpp
@@ -34,69 +34,196 @@ namespace gollnlp {
       
       auto p_li1 = new OptVariablesBlock(d.L_Line.size(), "p_li1", -3, 4);
       append_variables(p_li1);
-
       auto p_li2 = new OptVariablesBlock(d.L_Line.size(), "p_li2");
-       append_variables(p_li2);
-      auto q_li1 = new OptVariablesBlock(d.L_Line.size(), "q_li1");
-      auto q_li2 = new OptVariablesBlock(d.L_Line.size(), "q_li2");
-
-      append_variables(q_li1); append_variables(q_li2);
+      append_variables(p_li2);
       append_objterm(new DummySingleVarQuadrObjTerm("p_li1_sq", p_li1));
       append_objterm(new DummySingleVarQuadrObjTerm("p_li2_sq", p_li2));
+
+      auto q_li1 = new OptVariablesBlock(d.L_Line.size(), "q_li1");
+      auto q_li2 = new OptVariablesBlock(d.L_Line.size(), "q_li2");
+      append_variables(q_li1); append_variables(q_li2);
       append_objterm(new DummySingleVarQuadrObjTerm("q_li1_sq", q_li1));
       append_objterm(new DummySingleVarQuadrObjTerm("q_li2_sq", q_li2));
-
+ 
       auto p_ti1 = new OptVariablesBlock(d.T_Transformer.size(), "p_t1i");
       auto p_ti2 = new OptVariablesBlock(d.T_Transformer.size(), "p_ti2");
+      append_variables(p_ti1); 
+      append_objterm(new DummySingleVarQuadrObjTerm("p_ti1_sq", p_ti1));
+      append_variables(p_ti2); 
+      append_objterm(new DummySingleVarQuadrObjTerm("p_ti2_sq", p_ti2));
+
       auto q_ti1 = new OptVariablesBlock(d.T_Transformer.size(), "q_ti1");
       auto q_ti2 = new OptVariablesBlock(d.T_Transformer.size(), "q_ti2");
-      append_variables(p_ti1); append_variables(p_ti2); 
       append_variables(q_ti1); append_variables(q_ti2); 
-      append_objterm(new DummySingleVarQuadrObjTerm("p_ti1_sq", p_ti1));
-      append_objterm(new DummySingleVarQuadrObjTerm("p_ti2_sq", p_ti1));
       append_objterm(new DummySingleVarQuadrObjTerm("q_ti1_sq", q_ti1));
       append_objterm(new DummySingleVarQuadrObjTerm("q_ti2_sq", q_ti2));
       
-      auto SSh = new OptVariablesBlock(d.SSh_SShunt.size(), "SSh", d.SSh_Blb.data(), d.SSh_Bub.data());
-      SSh->set_start_to(d.SSh_B0.data());
-      append_variables(SSh);
-      append_objterm(new DummySingleVarQuadrObjTerm("SSh_sq", SSh));
+      // auto SSh = new OptVariablesBlock(d.SSh_SShunt.size(), "SSh", d.SSh_Blb.data(), d.SSh_Bub.data());
+      // SSh->set_start_to(d.SSh_B0.data());
+      // append_variables(SSh);
+      // append_objterm(new DummySingleVarQuadrObjTerm("SSh_sq", SSh));
       
-      auto p_g = new OptVariablesBlock(d.G_Generator.size(), "p_g", d.G_Plb.data(), d.G_Pub.data());
-      auto q_g = new OptVariablesBlock(d.G_Generator.size(), "q_g", d.G_Qlb.data(), d.G_Qub.data());
-      append_variables(p_g); append_variables(q_g); 
-      p_g->set_start_to(d.G_p0.data());
-      q_g->set_start_to(d.G_q0.data());
-      append_objterm(new DummySingleVarQuadrObjTerm("p_g_sq", p_g));
-      append_objterm(new DummySingleVarQuadrObjTerm("q_g_sq", q_g));
-      
+      // auto p_g = new OptVariablesBlock(d.G_Generator.size(), "p_g", d.G_Plb.data(), d.G_Pub.data());
+      // auto q_g = new OptVariablesBlock(d.G_Generator.size(), "q_g", d.G_Qlb.data(), d.G_Qub.data());
+      // append_variables(p_g); append_variables(q_g); 
+      // p_g->set_start_to(d.G_p0.data());
+      // q_g->set_start_to(d.G_q0.data());
+      // append_objterm(new DummySingleVarQuadrObjTerm("p_g_sq", p_g));
+      // append_objterm(new DummySingleVarQuadrObjTerm("q_g_sq", q_g));
+    
       //
       //constraints
       //
-      int one=1; double zero=1.;
+      int one=1; double zero=1., neghalf=-0.5;;
+      // lines power flows
+      {
+	// i=1 addpowerflowcon!(m, p_li[l,i], v_n[L_Nidx[l,i]], v_n[L_Nidx[l,3-i]], theta_n[L_Nidx[l,i]], theta_n[L_Nidx[l,3-i]], L[:G][l], -L[:G][l], -L[:B][l])
+	auto pf_cons1 = new PFConRectangular("p_li1_powerflow", d.L_Line.size(), 
+					     p_li1, v_n, theta_n,
+					     d.L_Nidx[0], d.L_Nidx[1]);
+	// i=2 addpowerflowcon!(m, p_li[l,i], v_n[L_Nidx[l,i]], v_n[L_Nidx[l,3-i]], theta_n[L_Nidx[l,i]], theta_n[L_Nidx[l,3-i]], L[:G][l], -L[:G][l], -L[:B][l])
+	auto pf_cons2 = new PFConRectangular("p_li2_powerflow", d.L_Line.size(), 
+					     p_li2, v_n, theta_n,
+					     d.L_Nidx[1], d.L_Nidx[0]);
+	//set the coefficients directly
+	DCOPY(&(pf_cons1->n), d.L_G.data(), &one, pf_cons1->get_A(), &one);
+	DCOPY(&(pf_cons2->n), d.L_G.data(), &one, pf_cons2->get_A(), &one);
+	
+	double *B=pf_cons1->get_B(), *LG=d.L_G.data();
+	for(int i=0; i<pf_cons1->n; i++) B[i]=-LG[i];
+	DCOPY(&(pf_cons2->n), B, &one, pf_cons2->get_B(), &one);
 
-      auto pf_cons = new PFConRectangular("p_li1_powerflow", d.L_Line.size(), 
-					  p_li1, v_n, theta_n,
-					  d.L_Nidx[0], d.L_Nidx[1]);
-      //set the coefficients directly
-      DCOPY(&(pf_cons->n), d.L_G.data(), &one, pf_cons->get_A(), &one);
+	double *C=pf_cons1->get_C(), *LB=d.L_B.data();
+	for(int i=0; i<pf_cons1->n; i++) C[i]=-LB[i];
+	DCOPY(&(pf_cons2->n), C, &one, pf_cons2->get_C(), &one);
+	
+	double* T=pf_cons1->get_T();
+	for(int i=0; i<pf_cons1->n; i++) T[i]=0.;
+	DCOPY(&(pf_cons2->n), T, &one, pf_cons2->get_T(), &one);
+	
+	append_constraints(pf_cons1);
+	append_constraints(pf_cons2);
+      }
+      {
+	// i=1 addpowerflowcon!(m, q_li[l,i], v_n[L_Nidx[l,i]], v_n[L_Nidx[l,3-i]], 
+	//                         theta_n[L_Nidx[l,i]], theta_n[L_Nidx[l,3-i]], 
+	//                          -L[:B][l]-L[:Bch][l]/2, L[:B][l], -L[:G][l])
+	auto pf_cons1 = new PFConRectangular("q_li1_powerflow", d.L_Line.size(), 
+					     q_li1, v_n, theta_n,
+					     d.L_Nidx[0], d.L_Nidx[1]);
+	// i=2 
+	auto pf_cons2 = new PFConRectangular("q_li2_powerflow", d.L_Line.size(), 
+					     q_li2, v_n, theta_n,
+					     d.L_Nidx[1], d.L_Nidx[0]);
+	
+	//set the coefficients directly
+	double *A=pf_cons1->get_A(), *LB=d.L_B.data();
+	for(int i=0; i<pf_cons1->n; i++) A[i]=-LB[i];
+	// A += -0.5*L_Bch
+	DAXPY(&(pf_cons1->n), &neghalf, d.L_Bch.data(), &one, A, &one); 
+	DCOPY(&(pf_cons2->n), A, &one, pf_cons2->get_A(), &one);
 
-      double *B=pf_cons->get_B(), *LG=d.L_G.data();
-      for(int i=0; i<pf_cons->n; i++) B[i]=-LG[i];
+	
+	DCOPY(&(pf_cons1->n), d.L_B.data(), &one, pf_cons1->get_B(), &one);
+	DCOPY(&(pf_cons2->n), d.L_B.data(), &one, pf_cons2->get_B(), &one);
 
-      double *C=pf_cons->get_C(), *LB=d.L_B.data();
-      for(int i=0; i<pf_cons->n; i++) C[i]=-LB[i];
+	double *C=pf_cons1->get_C(), *LG=d.L_G.data();
+	for(int i=0; i<pf_cons1->n; i++) C[i]=-LG[i];
+	DCOPY(&(pf_cons2->n), C, &one, pf_cons2->get_C(), &one);
+	
+	double* T=pf_cons1->get_T();
+	for(int i=0; i<pf_cons1->n; i++) T[i]=0.;
+	DCOPY(&(pf_cons2->n), T, &one, pf_cons2->get_T(), &one);
+	
+	append_constraints(pf_cons1);
+	append_constraints(pf_cons2);
+      }
+      // transformers power flows
+      {
+	// i=1 addpowerflowcon!(m, p_ti[t,1], v_n[T_Nidx[t,1]], v_n[T_Nidx[t,2]],
+	//		theta_n[T_Nidx[t,1]], theta_n[T_Nidx[t,2]],
+	//		T[:G][t]/T[:Tau][t]^2+T[:Gm][t], -T[:G][t]/T[:Tau][t], -T[:B][t]/T[:Tau][t], -T[:Theta][t])
+	auto pf_cons1 = new PFConRectangular("p_ti1_powerflow", d.T_Transformer.size(), 
+					     p_ti1, v_n, theta_n,
+					     d.T_Nidx[0], d.T_Nidx[1]);
+	//set the coefficients directly
+	double *A = pf_cons1->get_A(), *TG=d.T_G.data(), *TTau=d.T_Tau.data();
+	DCOPY(&(pf_cons1->n), d.T_Gm.data(), &one, A, &one);
+	for(int t=0; t<pf_cons1->n; t++) 
+	  A[t] += TG[t] / (TTau[t]*TTau[t]);
 
-      double* T=pf_cons->get_T();
-      for(int i=0; i<pf_cons->n; i++) T[i]=0.;
-      
-      append_constraints(pf_cons);
+	double *B=pf_cons1->get_B();
+	for(int t=0; t<pf_cons1->n; t++) 
+	  B[t]=-TG[t]/TTau[t];
+	
+	double *C=pf_cons1->get_C(), *TB=d.T_B.data();
+	for(int t=0; t<pf_cons1->n; t++) 
+	  C[t]=-TB[t]/TTau[t];
+
+	double *T=pf_cons1->get_T(), *TTheta=d.T_Theta.data();
+	for(int t=0; t<pf_cons1->n; t++) 
+	  T[t] = -TTheta[t];
+
+	// i=2 addpowerflowcon!(m, p_ti[t,2], v_n[T_Nidx[t,2]], v_n[T_Nidx[t,1]],
+	//		theta_n[T_Nidx[t,2]], theta_n[T_Nidx[t,1]],
+	//		T[:G][t], -T[:G][t]/T[:Tau][t], -T[:B][t]/T[:Tau][t], T[:Theta][t])
+	auto pf_cons2 = new PFConRectangular("p_ti2_powerflow", d.T_Transformer.size(), 
+					     p_ti2, v_n, theta_n,
+					     d.T_Nidx[1], d.T_Nidx[0]);
+	//set the coefficients directly
+	DCOPY(&(pf_cons2->n), d.T_G.data(), &one, pf_cons2->get_A(), &one);
+	DCOPY(&(pf_cons2->n), pf_cons1->get_B(), &one, pf_cons2->get_B(), &one);
+	DCOPY(&(pf_cons2->n), pf_cons1->get_C(), &one, pf_cons2->get_C(), &one);	
+	DCOPY(&(pf_cons2->n), TTheta, &one, pf_cons2->get_T(), &one);
+	
+	append_constraints(pf_cons1);
+	append_constraints(pf_cons2);
+      }
+      {
+      	// i=1 addpowerflowcon!(m, q_ti[t,1], v_n[T_Nidx[t,1]], v_n[T_Nidx[t,2]],
+	//		theta_n[T_Nidx[t,1]], theta_n[T_Nidx[t,2]],
+	//		-T[:B][t]/T[:Tau][t]^2-T[:Bm][t], T[:B][t]/T[:Tau][t], -T[:G][t]/T[:Tau][t], -T[:Theta][t])
+      	auto pf_cons1 = new PFConRectangular("q_ti1_powerflow", d.T_Transformer.size(), 
+      					     q_ti1, v_n, theta_n,
+      					     d.T_Nidx[0], d.T_Nidx[1]);
+      	//set the coefficients directly
+	double *A=pf_cons1->get_A(), *TB=d.T_B.data(), *TTau=d.T_Tau.data(), *TBM=d.T_Bm.data();
+	for(int t=0; t<pf_cons1->n; t++) 
+	  A[t] = -TBM[t]-TB[t]/(TTau[t]*TTau[t]);
+
+	double *B=pf_cons1->get_B();
+	for(int t=0; t<pf_cons1->n; t++) 
+	  B[t] = TB[t]/TTau[t];
+
+	double *C=pf_cons1->get_C(), *TG=d.T_G.data();
+	for(int t=0; t<pf_cons1->n; t++) 
+	  C[t] = -TG[t]/TTau[t];
+
+	double* T=pf_cons1->get_T(), *TTheta=d.T_Theta.data();
+	for(int t=0; t<pf_cons1->n; t++) T[t] = -TTheta[t];
+
+      	// i=2 addpowerflowcon!(m, q_ti[t,2], v_n[T_Nidx[t,2]], v_n[T_Nidx[t,1]],
+	//		theta_n[T_Nidx[t,2]], theta_n[T_Nidx[t,1]],
+	//		-T[:B][t], T[:B][t]/T[:Tau][t], -T[:G][t]/T[:Tau][t], T[:Theta][t])
+      	auto pf_cons2 = new PFConRectangular("q_ti2_powerflow", d.T_Transformer.size(), 
+      					     q_ti2, v_n, theta_n,
+      					     d.T_Nidx[1], d.T_Nidx[0]);
+	
+	A=pf_cons2->get_A();
+      	for(int i=0; i<pf_cons1->n; i++) A[i]=-TB[i];
+      	DCOPY(&(pf_cons2->n), pf_cons1->get_B(), &one, pf_cons2->get_B(), &one);
+      	DCOPY(&(pf_cons2->n), pf_cons1->get_C(), &one, pf_cons2->get_C(), &one);
+	DCOPY(&(pf_cons2->n), TTheta, &one, pf_cons2->get_T(), &one);
+	
+      	append_constraints(pf_cons1);
+      	append_constraints(pf_cons2);
+      }
 
       use_nlp_solver("ipopt");
       //set options
       set_solver_option("linear_solver", "ma57");
-      set_solver_option("print_timing_statistics", "yes");
-      set_solver_option("max_iter", 20);
+      //set_solver_option("print_timing_statistics", "yes");
+      set_solver_option("max_iter", 250);
       //prob.set_solver_option("print_level", 6);
       bool bret = optimize("ipopt");
 
