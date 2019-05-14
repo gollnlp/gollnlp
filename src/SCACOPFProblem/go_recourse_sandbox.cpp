db8c854d2e65c7f15bf1fc57b4a1b8b52a72e700
Cosmin G. Petra <petra1@llnl.gov>
2019-05-13 17:02:16 -0700

    sync - dirty

--- src/SCACOPFProblem/go_recourse_sandbox.cpp
+++ src/SCACOPFProblem/go_recourse_sandbox.cpp
@@ -23,12 +23,16 @@ int main(int argc, char *argv[])
 		 root+"case.inl",
 		 root+scen+"case.con");
   //net 07R scenario 9
-  std::vector<int> cont_list = {426,//, //line/trans conting, penalty $417
-				960, // gen conting, penalty $81,xxx
-				//961,
-				963};// gen conting, penalty $52,xxx
+  std::vector<int> cont_list = {
+    //426,//, //line/trans conting, penalty $417
+    //960, // gen conting, penalty $81,xxx
+    //961,
+    963
+  };// gen conting, penalty $52,xxx
   
+
   SCMasterProblem master_prob(d, cont_list);
+  //SCACOPFProblem master_prob(d);
   master_prob.default_assembly();
 
   //
@@ -44,36 +48,58 @@ int main(int argc, char *argv[])
 	 master_prob.objective_value());
 
 
-  OptProblem p;
-  auto p_g0 = new OptVariablesBlock(d.G_Generator.size(), "p_g_0", 
-				    d.G_Plb.data(), d.G_Pub.data());
-  p.append_variables(p_g0);
-  p_g0->set_start_to(master_prob.vars_block("p_g_0")->x);
-  p_g0->xref = p_g0->x; 
+  //
+  //phase 2
+  //
 
-  printf("creating recourse objective\n");
-  SCRecourseObjTerm* rec;
-  p.append_objterm(rec=new SCRecourseObjTerm(d,
-					     p_g0,
-					     NULL, //master_prob.v_n0_vars(),
-					     cont_list));
-  p.use_nlp_solver("ipopt");
-  p.set_solver_option("linear_solver", "ma57"); 
-  
-  //p.set_solver_option("derivative_test", "first-order");
-  //p.set_solver_option("derivative_test_perturbation",  1e-2);
-  //p.set_solver_option("derivative_test_tol", 1e-2);
 
-  p.set_solver_option("mu_init", 1e-6);
-  p.set_solver_option("mu_target", 1e-6);
-  p.set_solver_option("tol", 1e-4);
-  p.set_solver_option("hessian_approximation", "limited-memory");
-  //p.set_solver_option("accept_every_trial_step", "yes");
-  //https://arxiv.org/pdf/1901.05682.pdf
-  p.set_solver_option("limited_memory_initialization", "scalar1");
-  p.set_solver_option("limited_memory_max_history", 0);
-  p.set_solver_option("limited_memory_init_val", 100.);
-  p.optimize("ipopt");
+  if(false) {
+    SCMasterProblem p(d, cont_list);    
+    auto p_g0 = new OptVariablesBlock(d.G_Generator.size(), "p_g_0", 
+				      d.G_Plb.data(), d.G_Pub.data());
+    p.append_variables(p_g0);
+    p_g0->set_start_to(master_prob.vars_block("p_g_0")->x);
+    p_g0->xref = p_g0->x; 
+    
+    printf("creating recourse objective\n");
+    SCRecourseObjTerm* rec;
+    p.append_recourse_obj(rec=new SCRecourseObjTerm(d,
+						    p_g0,
+						    NULL, //master_prob.v_n0_vars(),
+						    cont_list));
+    p.use_nlp_solver("ipopt");
+    p.set_solver_option("linear_solver", "ma57"); 
+    
+    //p.set_solver_option("derivative_test", "first-order");
+    //p.set_solver_option("derivative_test_perturbation",  1e-2);
+    //p.set_solver_option("derivative_test_tol", 1e-3);
+    
+    p.set_solver_option("mu_init", 1e-6);
+    p.set_solver_option("mu_target", 1e-6);
+    p.set_solver_option("tol", 1e-4);
+    
+    if(false) {
+      p.set_solver_option("hessian_approximation", "limited-memory");
+      //https://arxiv.org/pdf/1901.05682.pdf
+      p.set_solver_option("limited_memory_initialization", "scalar1");
+      //p.set_solver_option("limited_memory_max_history", 0);
+      p.set_solver_option("limited_memory_init_val", 100.);
+      p.set_solver_option("print_level", 5);
+    }
+    p.optimize("ipopt");
+  } else {
+    SCRecourseObjTerm* rec;
+    master_prob.append_recourse_obj(rec=new SCRecourseObjTerm(d, 
+    							 master_prob.vars_block("p_g_0"), 
+    							 NULL, 
+    							 cont_list));
+    master_prob.set_solver_option("mu_init", 1e-9);
+    master_prob.set_solver_option("bound_push", 1e-18);
+    master_prob.set_solver_option("slack_bound_push", 1e-18);
+    master_prob.problem_changed();
+    //master_prob.reoptimize("ipopt");
+    bret = master_prob.reoptimize(OptProblem::primalDualRestart); //warm_start_target_mu
+  }
   
   printf("Test finished\n");
   
