1b8e9f384cf12e8932b90dd71ec8a7aea9c544b6
Cosmin G. Petra <petra1@llnl.gov>
2019-07-01 08:41:21 -0700

    added measure time in the timer

--- src/Utils/goTimer.hpp
+++ src/Utils/goTimer.hpp
@@ -22,6 +22,19 @@ public:
   //returns the elapsed time (accumulated between start/stop) in seconds
   inline double getElapsedTime() const { return tmElapsed; }
 
+  //returns the elapsed time since start, the timer is not stopped
+  inline double measureElapsedTime() 
+  {
+    double tm;
+#ifdef GOLLNLP_USE_MPI
+    tm= ( MPI_Wtime()-tmStart );
+#else
+    gettimeofday(&tv, NULL);
+    tm = ( static_cast<double>(tv.tv_sec) + static_cast<double>(tv.tv_usec)/1000000.0  - tmStart );
+#endif
+    return tm;
+  }
+
   inline void start() 
   {
 #ifdef GOLLNLP_USE_MPI 
