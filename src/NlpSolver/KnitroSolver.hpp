e181b0216e834494b0b0f69e21ffae460615826d
Ignacio Aravena <aravenasolis1@llnl.gov>
2019-08-27 09:08:35 -0700

    still compiling but not linking

--- src/NlpSolver/KnitroSolver.hpp
+++ src/NlpSolver/KnitroSolver.hpp
@@ -4,12 +4,12 @@
 #include "OptProblem.hpp"
 #include "NlpSolver.hpp"
 
-#include "KTRProblem.h"
 #include "KTRSolver.h"
+#include "KTRProblem.h"
 
 #include "goTimer.hpp"
 
-using namespace Knitro;
+using namespace knitro;
 
 #include <iostream>
 
@@ -42,7 +42,9 @@ public:
   double evaluateFC(const double* const x, double* const c, double* const objGrad,
                     double* const jac) {
     prob->eval_cons(x, true, c);
-    return prob->eval_obj(x, true, obj_value);
+    double obj_value = DBL_MAX;
+    prob->eval_obj(x, true, obj_value);
+    return obj_value;
   }
   
   /* gradient evaluator */
@@ -73,18 +75,18 @@ private:
   
   virtual void setVariableProperties() {
     std::vector<double> x_bnd(getNumVars());
-    prob->fill_vars_lower_bounds(x_bnd);
+    prob->fill_vars_lower_bounds(&(x_bnd[0]));
     setVarLoBnds(x_bnd);
-    prob->fill_vars_upper_bounds(x_bnd);
+    prob->fill_vars_upper_bounds(&(x_bnd[0]));
     setVarUpBnds(x_bnd);
   }
   
   virtual void setConstraintProperties() {
     std::vector<double> bnds(getNumCons());
-    setConTypes(knitro::KTREnums::ConstraintType::General); // PENDING: most constrains are actually quadratic -- to be revised
-    prob->fill_cons_lower_bounds(bnds);
+    setConTypes(knitro::KTREnums::ConstraintType::ConGeneral); // PENDING: most constrains are actually quadratic -- to be revised
+    prob->fill_cons_lower_bounds(&(bnds[0]));
     setConLoBnds(bnds);
-    prob->fill_cons_upper_bounds(bnds);
+    prob->fill_cons_upper_bounds(&(bnds[0]));
     setConUpBnds(bnds);
   }
 
@@ -95,7 +97,7 @@ private:
       auto jac_nnz = getNNZJ();
       std::vector<int> jac_iRow(jac_nnz);
       std::vector<int> jac_jCol(jac_nnz);
-      prob->eval_Jaccons(NULL, false, jac_nnz, jac_iRow, jac_jCol, NULL);
+      prob->eval_Jaccons(NULL, false, jac_nnz, &(jac_iRow[0]), &(jac_jCol[0]), NULL);
       setJacIndexCons(jac_iRow);
       setJacIndexVars(jac_jCol);
     }
@@ -105,8 +107,8 @@ private:
       auto hess_nnz = getNNZH();
       std::vector<int> hess_iRow(hess_nnz);
       std::vector<int> hess_jCol(hess_nnz);
-      prob->eval_HessLagr(NULL, false, 1.0, getNumCons(), NULL, false,
-                          hess_nnz, hess_iRow, hess_jCol, NULL);
+      prob->eval_HessLagr(NULL, false, 1.0, NULL, false,
+                          hess_nnz, &(hess_iRow[0]), &(hess_jCol[0]), NULL);
       setHessIndexRows(hess_iRow);
       setHessIndexCols(hess_jCol);
     }
@@ -122,7 +124,7 @@ private:
     // setting primal starting point
     {
       std::vector<double> x0(nvars);
-      if(prob->fill_primal_start(x0)) {
+      if(prob->fill_primal_start(&(x0[0]))) {
         setXInitial(x0);
       }
     }
@@ -134,13 +136,13 @@ private:
       std::vector<double> lambda0(ncons + nvars);
       std::vector<double> lambda0_cons(lambda0.cbegin(), lambda0.cbegin()+ncons);
       std::vector<double> lambda0_bnds(lambda0.cbegin()+ncons, lambda0.cbegin()+ncons+nvars);
-      if(!prob->fill_dual_cons_start(lambda0_cons)) {
+      if(!prob->fill_dual_cons_start(&(lambda0_cons[0]))) {
         return;
       }
       {
         std::vector<double> z_L(nvars);
 	std::vector<double> z_U(nvars);
-	if(!prob->fill_dual_bounds_start(z_L, z_U)) {
+	if(!prob->fill_dual_bounds_start(&(z_L[0]), &(z_U[0]))) {
 	  return;
         }
         for(int i=0; i<nvars; i++){
@@ -159,9 +161,11 @@ private:
   KnitroNlp(const KnitroNlp&);
   KnitroNlp& operator=(const KnitroNlp&);
 
-}
+};
 
-class KnitroSolver : public NlpSolver : {
+class KnitroSolver : public NlpSolver
+{
+public:
   KnitroSolver(OptProblem* p_) : NlpSolver(p_) {}
   virtual ~KnitroSolver() {}
   
@@ -170,33 +174,34 @@ class KnitroSolver : public NlpSolver : {
     app = new knitro::KTRSolver(knitro_nlp_spec);
     return true;
   }
+
+  virtual bool set_start_type(OptProblem::RestartType t) { return true;}
   
-  virtual bool optimize() {
+  virtual int optimize() {
     
     // solve optimization problem
-    int status = app->asolve();
-    if(status == knitro::KN_RC_OPTIMAL_OR_SATISFACTORY ||
-       status == knitro::KN_RC_NEAR_OPT) {
+    int status = app->solve();
+    if(status == KN_RC_OPTIMAL_OR_SATISFACTORY ||
+       status == KN_RC_NEAR_OPT) {
     } else {
       printf("Knitro solve FAILED with status %d!!!\nSee https://www.artelys.com/docs/knitro/3_referenceManual/returnCodes.html#returncodes\n", status);
       return false;
     }
     
     // record solution -- knitro does not call a finalize procedure
-    OptProblem& p = knitro_nlp_spec->prob;
-    p->set_obj_value(app->getObjValue());
-    p->set_num_iters(app->getNumberIters());
+    prob->set_obj_value(app->getObjValue());
+    prob->set_num_iters(app->getNumberIters());
     {
-      std::vector<double>& x_sol = app->getXValues();
-      p->set_primal_vars(x_sol);
+      const std::vector<double>& x_sol = app->getXValues();
+      prob->set_primal_vars(&(x_sol[0]));
     }
     {
-      auto nvars = app->getNumVars();
-      auto ncons = app->getNumCons();
-      std::vector<double>& lambda_sol = app->getLambdaValues();
+      auto nvars = knitro_nlp_spec->getNumVars();
+      auto ncons = knitro_nlp_spec->getNumCons();
+      const std::vector<double>& lambda_sol = app->getLambdaValues();
       std::vector<double> lambda_cons(lambda_sol.cbegin(), lambda_sol.cbegin()+ncons);
       std::vector<double> lambda_bnds(lambda_sol.cbegin()+ncons, lambda_sol.cbegin()+ncons+nvars);
-      p->set_duals_vars_cons(lambda_cons);
+      prob->set_duals_vars_cons(&(lambda_cons[0]));
       std::vector<double> z_L(nvars);
       std::vector<double> z_U(nvars);
       for(int i=0; i<nvars; i++){
@@ -208,11 +213,13 @@ class KnitroSolver : public NlpSolver : {
           z_U[i] = lambda_bnds[i];
         }
       }
-      p->set_duals_vars_bounds(z_L, z_U);
+      prob->set_duals_vars_bounds(&(z_L[0]), &(z_U[0]));
     }
     return true;
     
   }
+
+  virtual int reoptimize() { return optimize(); }
   
   virtual bool set_option(const std::string& name, int value) {
     app->setParam(name, value);
@@ -230,9 +237,10 @@ class KnitroSolver : public NlpSolver : {
   };
   
 private:
+  gollnlp::OptProblem* prob;
   gollnlp::KnitroNlp* knitro_nlp_spec;
   knitro::KTRSolver* app;
-}
+};
 
 } //endnamespace
 #endif
