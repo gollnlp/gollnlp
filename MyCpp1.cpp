6fa2bfed48bc8a8085242fbb267cd126db6f90c4
Cosmin G. Petra <petra1@llnl.gov>
2019-09-13 14:51:47 -0700

    timestamps

--- MyCpp1.cpp
+++ MyCpp1.cpp
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
   int retcode=0;
   gollnlp::goTimer ttot; ttot.start();
 
-  std::cout << "MyExe1 - v. Sept 13, 2019 - 12:17pm" << std::endl;
+  std::cout << "MyExe1 - v. Sept 13, 2019 - 02:51pm" << std::endl;
 #ifdef DEBUG
   std::cout << "DEBUG build !!!!" << std::endl;
 #endif
