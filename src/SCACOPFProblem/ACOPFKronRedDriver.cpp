f3930f35565ba23c841fa8a23b745f9140612b71
Cosmin Petra <petra1@llnl.gov>
2020-01-31 21:43:02 -0800

    eod - updated build system and the kronreduction driver

--- src/SCACOPFProblem/ACOPFKronRedDriver.cpp
+++ src/SCACOPFProblem/ACOPFKronRedDriver.cpp
@@ -5,6 +5,11 @@ using namespace gollnlp;
 
 #include "goTimer.hpp"
 
+#include <complex>
+#include <cmath>
+#include <iostream>
+#include <cassert>
+
 // INST_DIR=/home/petra1/work/projects/gocompet/goinstances/trial1/T1S3_Offline/Network_01O-3; 
 // CON=$INST_DIR/scenario_1/case.con; INL=$INST_DIR/case.inl; RAW=$INST_DIR/scenario_1/case.raw; ROP=$INST_DIR/case.rop
 // ./src/SCACOPFProblem/acopf_kron_driver.exe $CON $INL $RAW $ROP
@@ -32,14 +37,44 @@ int main(int argc, char *argv[])
     prob.assemble();
 
     
-    prob.use_nlp_solver("ipopt");
-    bool bret = prob.optimize("ipopt");
-    if(!bret) retcode=-3;
+    //prob.use_nlp_solver("ipopt");
+    //bool bret = prob.optimize("ipopt");
+    //if(!bret) retcode=-3;
 
   } else {
     printf("Usage: %s con_file inl_file raw_file rop_file\n", argv[0]);
     retcode=-1;
   }
+
+  //using namespace std::complex_literals;
+  int vsize = 10;
+  assert(vsize>=1);
+
+  complex<double>* va = new complex<double>[vsize];
+  assert(va);
+  double* vad = new double[vsize*2];
+  assert(vad);
+
+  if(true){
+    complex<double> a = 1.; a.imag(1.17);
+    vector<complex<double> > v(vsize, a);
+    
+    
+    v[vsize-1] = exp(v[0]);
+    
+    for(auto& e : v) cout << e << " "; cout << endl;
+    
+
+    for(int i=0; i<vsize; i++)
+      memcpy(va, &v[0], vsize*sizeof(complex<double>));
+    memcpy(vad, va,   vsize*2*sizeof(double));
+  }
+
+  for(int i=0; i<vsize; i++) cout << va[i].real() << " " << va[i].imag() << " "; cout << endl;
+  for(int i=0; i<2*vsize; i++) cout << vad[i] << " "; cout << endl;
+  
+  delete[] va;
+  delete[] vad;
 
   MPI_Finalize();
   return retcode;
