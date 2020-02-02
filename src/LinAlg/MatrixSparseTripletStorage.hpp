45249e0212b648b24489825e4b1509fe343d6631
Cosmin G. Petra <petra1@llnl.gov>
2020-02-02 15:29:02 -0800

    MatrixSparseTriplet : added sort_indexes and sum_up_duplicates

--- src/LinAlg/MatrixSparseTripletStorage.hpp
+++ src/LinAlg/MatrixSparseTripletStorage.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <complex>
 
+#include <numeric>
 #include <algorithm>
 
 //container for sparse matrices in triplet format; implements minimal functionality for matrix ops
@@ -33,6 +34,9 @@ public:
 
   //sorts the (i,j) in increasing order of 'i' and for equal 'i's in increasing order of 'j'
   //Complexity: n*log(n)
+  //
+  // Warning: irow_, jcol_, and values_ pointers will changes inside this method. Corresponding
+  // accessor methods i(), j(), M() should be called again to get the correct pointers
   void sort_indexes() {
     std::vector<Tidx> vIdx(nnz);
     std::iota(vIdx.begin(), vIdx.end(), 0);
@@ -43,9 +47,71 @@ public:
 	   return jcol_[i1]<jcol_[i2];
 	 });
 
-    //for(int itnz=0; itnz
+    //permute irow, jcol, and M using additional storage
+
+    //irow and jcol can use the same  buffer 
+    {
+      Tidx* buffer = new Tidx[nnz];
+      for(int itnz=0; itnz<nnz; itnz++)
+	buffer[itnz] = irow_[vIdx[itnz]];
+      
+      //avoid copy back
+      Tidx* buffer2 = irow_;
+      irow_ = buffer;
+      buffer = buffer2; 
+      
+      for(int itnz=0; itnz<nnz; itnz++)
+	buffer[itnz] = jcol_[vIdx[itnz]];
+      
+      delete[] jcol_;
+      jcol_ = buffer;
+    }
+
+    //M
+    {
+      Tval* buffer = new Tval[nnz];
+      
+      for(int itnz=0; itnz<nnz; itnz++)
+	buffer[itnz] = values_[vIdx[itnz]];
+
+      delete[] values_;
+      values_ = buffer;
+    }
   }
 
+  //add elements with identical (i,j) and update nnz_, irow_, jcol_, and values_ array accordingly
+  // Precondition: (irow_,jcol_) are assumed to be sorted (see sort_indexes())
+  void sum_up_duplicates()
+  {
+    if(nnz_<=0) return;
+    Tidx itleft=0, itright=1;
+    Tidx currI=irow_[0], currJ=jcol_[0];
+    Tval val1 = values_[0];
+    
+    while(itright<nnz) {
+      values_[itleft] = val1;
+      
+      while(itright<nnz && irow_[itright]==currI && jcol_[itright]==currJ) {
+	values_[itleft] += values_[itright];
+	itright++;
+      }
+      irow_[itleft] = currI;
+      jcol_[itleft] = currJ;
+
+      if(itright<nnz) {
+	currI = irow_[itright];
+	currJ = jcol_[itright];
+	val1 = values_[itright];
+	itright++;
+      }
+      itleft++;
+      assert(itleft<=nnz);
+      assert(itleft<itright);
+    }
+    
+    nnz_ = itleft;
+  }
+  
   Tidx n() const { return nrows_; }
   Tidx m() const { return ncols_; }
   Tidx nnz() const { return nnz_; }
