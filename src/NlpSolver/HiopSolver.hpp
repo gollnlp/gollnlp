6d9351a488183f08c3ef9cda773d6cb1a9611211
Cosmin G. Petra <petra1@llnl.gov>
2020-03-27 22:39:01 -0700

    MDS changes
    and indentation changed

--- src/NlpSolver/HiopSolver.hpp
+++ src/NlpSolver/HiopSolver.hpp
@@ -1,7 +1,7 @@
 #ifndef GOLLNLP_HIOPSOLVER
 #define GOLLNLP_HIOPSOLVER
 
-#include "OptProblem.hpp"
+#include "OptProblemMDS.hpp"
 #include "NlpSolver.hpp"
 
 #include "hiopNlpFormulation.hpp"
@@ -16,193 +16,207 @@
 
 namespace gollnlp {
 
-  class HiopNlp : public hiop::hiopInterfaceMDS
-{
-public:
-  /**constructor */
-  HiopNlp(OptProblem* p) : prob(p)
-  { 
-  } 
-
-  /** default destructor */
-  virtual ~HiopNlp()
+  class HiopNlpMDS : public hiop::hiopInterfaceMDS
   {
-  }
+  public:
+    /**constructor */
+    HiopNlpMDS(OptProblemMDS* p) : prob(p)
+    { 
+    } 
 
-  bool get_prob_sizes(long long& n, long long& m)
-  { 
-    n = prob->get_num_variables();
-    m = prob->get_num_constraints();
-    return true; 
-  }
+    /** default destructor */
+    virtual ~HiopNlpMDS()
+    {
+    }
+    
+    bool get_prob_sizes(long long& n, long long& m)
+    { 
+      n = prob->get_num_variables();
+      m = prob->get_num_constraints();
+      return true; 
+    }
+    
+    bool get_vars_info(const long long& n, double *xlow, double* xupp, NonlinearityType* type)
+    {
+      assert(n == prob->get_num_variables());
+      prob->fill_vars_lower_bounds(xlow);
+      prob->fill_vars_upper_bounds(xupp);
+      for(int i=0; i<n; i++) type[i]=hiopNonlinear;
+      return true;
+    }
 
-  bool get_vars_info(const long long& n, double *xlow, double* xupp, NonlinearityType* type)
-  {
-    assert(n == prob->get_num_variables());
-    prob->fill_vars_lower_bounds(xlow);
-    prob->fill_vars_upper_bounds(xupp);
-    for(int i=0; i<n; i++) type[i]=hiopNonlinear;
-    return true;
-  }
+    bool get_cons_info(const long long& m, double* clow, double* cupp, NonlinearityType* type)
+    {
+      assert(m == prob->get_num_constraints());
+      prob->fill_cons_lower_bounds(clow);
+      prob->fill_cons_upper_bounds(cupp);
+      for(int i=0; i<m; i++) type[i]=hiopNonlinear;
+      return true;
+    }
 
-  bool get_cons_info(const long long& m, double* clow, double* cupp, NonlinearityType* type)
-  {
-    assert(m == prob->get_num_constraints());
-    prob->fill_cons_lower_bounds(clow);
-    prob->fill_cons_upper_bounds(cupp);
-    for(int i=0; i<m; i++) type[i]=hiopNonlinear;
-    return true;
-  }
+    bool get_sparse_dense_blocks_info(int& nx_sparse, int& nx_dense,
+				      int& nnz_sparse_Jace, int& nnz_sparse_Jaci,
+				      int& nnz_sparse_Hess_Lagr_SS, int& nnz_sparse_Hess_Lagr_SD)
+    {
+      int n = prob->get_num_variables();
+      int m = prob->get_num_constraints();
+    
+      prob->get_num_variables_dense_sparse(nx_dense, nx_sparse);
+      assert(n == nx_dense+nx_sparse);
+    
+      //int nnz_jac_g = prob->get_nnzJaccons();
+      //int nnz_h_lag = prob->get_nnzHessLagr();
 
-  bool get_sparse_dense_blocks_info(int& nx_sparse, int& nx_dense,
-				    int& nnz_sparse_Jace, int& nnz_sparse_Jaci,
-				    int& nnz_sparse_Hess_Lagr_SS, int& nnz_sparse_Hess_Lagr_SD)
-  {
-    return true;
-  }
+ 
+    
+      return false;
+    }
 
-  bool eval_f(const long long& n, const double* x, bool new_x, double& obj_value)
-  {
-    assert(prob->get_num_variables() == n);
-    return prob->eval_obj(x, new_x, obj_value);
-    return true;
-  }
+    bool eval_f(const long long& n, const double* x, bool new_x, double& obj_value)
+    {
+      assert(prob->get_num_variables() == n);
+      return prob->eval_obj(x, new_x, obj_value);
+    }
 
-  bool eval_cons(const long long& n, const long long& m, 
-		 const long long& num_cons, const long long* idx_cons,  
-		 const double* x, bool new_x, double* cons)
-  {
-    if(num_cons==0) return true;
-    assert(num_cons==n);
-    return prob->eval_cons(x, new_x, cons);
-  }
+    bool eval_cons(const long long& n, const long long& m, 
+		   const long long& num_cons, const long long* idx_cons,  
+		   const double* x, bool new_x, double* cons)
+    {
+      if(num_cons==0) return true;
+      assert(num_cons==n);
+      return prob->eval_cons(x, new_x, cons);
+    }
 
-  bool eval_grad_f(const long long& n, const double* x, bool new_x, double* gradf)
-  {
-    return prob->eval_gradobj(x, new_x, gradf);
-    return true;
-  }
+    bool eval_grad_f(const long long& n, const double* x, bool new_x, double* gradf)
+    {
+      return prob->eval_gradobj(x, new_x, gradf);
+      return true;
+    }
 
-  bool eval_Jac_cons(const long long& n, const long long& m, 
-		     const long long& num_cons, const long long* idx_cons,
-		     const double* x, bool new_x,
-		     const long long& nsparse, const long long& ndense, 
-		     const int& nnzJacS, int* iJacS, int* jJacS, double* MJacS, 
-		     double** JacD)
-  {
-    return true;
-  }
+    bool eval_Jac_cons(const long long& n, const long long& m, 
+		       const long long& num_cons, const long long* idx_cons,
+		       const double* x, bool new_x,
+		       const long long& nsparse, const long long& ndense, 
+		       const int& nnzJacS, int* iJacS, int* jJacS, double* MJacS, 
+		       double** JacD)
+    {
+      return true;
+    }
 
-  bool eval_Hess_Lagr(const long long& n, const long long& m, 
-			      const double* x, bool new_x, const double& obj_factor,
-			      const double* lambda, bool new_lambda,
-			      const long long& nsparse, const long long& ndense, 
-			      const int& nnzHSS, int* iHSS, int* jHSS, double* MHSS, 
-			      double** HDD,
-			      int& nnzHSD, int* iHSD, int* jHSD, double* MHSD)
-  {
-    return true;
-  }
+    bool eval_Hess_Lagr(const long long& n, const long long& m, 
+			const double* x, bool new_x, const double& obj_factor,
+			const double* lambda, bool new_lambda,
+			const long long& nsparse, const long long& ndense, 
+			const int& nnzHSS, int* iHSS, int* jHSS, double* MHSS, 
+			double** HDD,
+			int& nnzHSD, int* iHSD, int* jHSD, double* MHSD)
+    {
+      return true;
+    }
 
-  bool get_starting_point(const long long& global_n, double* x0)
-  {
-    if(!prob->fill_primal_start(x0)) return false;
-    return true;
-  }
-private:
-  OptProblem* prob;
+    bool get_starting_point(const long long& global_n, double* x0)
+    {
+      if(!prob->fill_primal_start(x0)) return false;
+      return true;
+    }
+  private:
+    OptProblemMDS* prob;
 
-  /**@name Methods to block default compiler methods.
-   */
-  //@{
-  HiopNlp();
-  HiopNlp(const HiopNlp&);
-  HiopNlp& operator=(const HiopNlp&);
-  //@}
-};
-
-class HiopSolver : public NlpSolver {
-public:
-  HiopSolver(OptProblem* p_) : NlpSolver(p_), app_status(Invalid_Option) {}
-  virtual ~HiopSolver() {}
-
-  virtual bool set_start_type(OptProblem::RestartType t)
-  {
-    // if(t==OptProblem::primalDualRestart) {
-    //   app->Options()->SetStringValue("warm_start_init_point", "yes");
-    // 	ipopt_nlp_spec->set_advanced_primaldual_restart(false);
-    // } else {
-    //   if(t==OptProblem::primalRestart)
-    // 	app->Options()->SetStringValue("warm_start_init_point", "no");
-    //   else { //	advancedPrimalDualRestart	
-    // 	app->Options()->SetStringValue("warm_start_init_point", "yes");
-    // 	app->Options()->SetStringValue("warm_start_entire_iterate", "yes");
-    // 	ipopt_nlp_spec->set_advanced_primaldual_restart(true);
-    //   }
-    // }
-    return true;
-  }
-
-
-  virtual bool initialize() {
-
-    hiop_nlp_spec = new gollnlp::HiopNlp(prob);
-    return true;
-  }
-
-  OptimizationStatus return_code() const {
-    return app_status;
-  }
-
-  virtual int optimize() {
-    hiop::hiopNlpMDS nlp(*hiop_nlp_spec);
-    hiop::hiopAlgFilterIPMNewton solver(&nlp);
-    hiop::hiopSolveStatus status = solver.run();
-    double objective = solver.getObjective();
-
-    // Ask Ipopt to solve the problem
-    // app_status = app->OptimizeTNLP(ipopt_nlp_spec);
-
-    // if (app_status == Ipopt::Solve_Succeeded || 
-    // 	app_status == Ipopt::Solved_To_Acceptable_Level) {
-    //   //|| app_status == Ipopt::User_Requested_Stop) {
-    //   return true;
-    // }
-    // else {
-    //   if(app_status != Ipopt::User_Requested_Stop)
-    // 	printf("Ipopt solve FAILED with status %d!!!\n", app_status);
-    //   return false;
-    // }
-    return true;
-  }
-
-  virtual int reoptimize() {
-    assert(false);
-    return false;
-  }
-
-  virtual bool set_option(const std::string& name, int value)
-  {
-    //app->Options()->SetIntegerValue(name, value);
-    assert(false);return true;
-  }
-  virtual bool set_option(const std::string& name, double value)
-  {
-    //app->Options()->SetNumericValue(name, value);
-    assert(false);return true;
-  };
-  virtual bool set_option(const std::string& name, const std::string& value)
-  {
-    //app->Options()->SetStringValue(name, value);
-    assert(false);return true;
+    /** Block default compiler methods.
+     */
+    HiopNlpMDS();
+    HiopNlpMDS(const HiopNlpMDS&);
+    HiopNlpMDS& operator=(const HiopNlpMDS&);
   };
 
-private:
-  HiopNlp* hiop_nlp_spec;
-  OptimizationStatus app_status;
-};
+  class HiopSolverMDS : public NlpSolver {
+  public:
+    HiopSolverMDS(OptProblemMDS* p_) : NlpSolver(p_), app_status(OptProblem::Invalid_Option) {}
+    virtual ~HiopSolverMDS() {}
+
+    virtual bool set_start_type(OptProblem::RestartType t)
+    {
+      assert(false && "not yet implemented");
+      // if(t==OptProblem::primalDualRestart) {
+      //   app->Options()->SetStringValue("warm_start_init_point", "yes");
+      // 	ipopt_nlp_spec->set_advanced_primaldual_restart(false);
+      // } else {
+      //   if(t==OptProblem::primalRestart)
+      // 	app->Options()->SetStringValue("warm_start_init_point", "no");
+      //   else { //	advancedPrimalDualRestart	
+      // 	app->Options()->SetStringValue("warm_start_init_point", "yes");
+      // 	app->Options()->SetStringValue("warm_start_entire_iterate", "yes");
+      // 	ipopt_nlp_spec->set_advanced_primaldual_restart(true);
+      //   }
+      // }
+      return true;
+    }
 
 
+    virtual bool initialize() {
+
+      //this->prob is inherited from base NlpSolver and has the base type, OptProblem
+      OptProblemMDS* probMDS = dynamic_cast<OptProblemMDS*>(this->prob);
+      if(probMDS==NULL) {
+	printf("HiopSolver did not received an MDS OptProblem and cannot initialize\n");
+	return false;
+      }
+    
+      hiop_nlp_spec = new gollnlp::HiopNlpMDS(probMDS);
+      return true;
+    }
+
+    OptimizationStatus return_code() const {
+      return app_status;
+    }
+
+    virtual int optimize() {
+      hiop::hiopNlpMDS nlp(*hiop_nlp_spec);
+      hiop::hiopAlgFilterIPMNewton solver(&nlp);
+      hiop::hiopSolveStatus status = solver.run();
+      double objective = solver.getObjective();
+
+      // Ask Ipopt to solve the problem
+      // app_status = app->OptimizeTNLP(ipopt_nlp_spec);
+
+      // if (app_status == Ipopt::Solve_Succeeded || 
+      // 	app_status == Ipopt::Solved_To_Acceptable_Level) {
+      //   //|| app_status == Ipopt::User_Requested_Stop) {
+      //   return true;
+      // }
+      // else {
+      //   if(app_status != Ipopt::User_Requested_Stop)
+      // 	printf("Ipopt solve FAILED with status %d!!!\n", app_status);
+      //   return false;
+      // }
+      return true;
+    }
+
+    virtual int reoptimize() {
+      assert(false && "not yet implemented");
+      return false;
+    }
+
+    virtual bool set_option(const std::string& name, int value)
+    {
+      //app->Options()->SetIntegerValue(name, value);
+      assert(false);return true;
+    }
+    virtual bool set_option(const std::string& name, double value)
+    {
+      //app->Options()->SetNumericValue(name, value);
+      assert(false);return true;
+    };
+    virtual bool set_option(const std::string& name, const std::string& value)
+    {
+      //app->Options()->SetStringValue(name, value);
+      assert(false);return true;
+    };
+
+  private:
+    gollnlp::HiopNlpMDS* hiop_nlp_spec;
+    OptimizationStatus app_status;
+  };
 
 } //endnamespace
 #endif
